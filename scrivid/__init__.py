ff729f6b20daf092c6ca4445eaf080ad22bee175
TNTMaster370 <trystynmcote@gmail.com>
2023-11-01 20:57:51 -0600

    Changed VisibilityStatus to make it accessible externally.

--- scrivid/__init__.py
+++ scrivid/__init__.py
@@ -1,6 +1,6 @@
 from . import errors
 from ._file_objects import FileReference, HideAdjustment, image_reference, ImageFileReference, ImageReference, \
-    properties, Properties, RootAdjustment, ShowAdjustment
+    properties, Properties, RootAdjustment, ShowAdjustment, VisibilityStatus
 from ._motion_tree import dump, nodes as motion_nodes, parse, walk
 from .compile_video import compile_video
 from .metadata import Metadata
@@ -8,4 +8,4 @@ from .metadata import Metadata
 
 __all__ = ["compile_video", "dump", "errors", "FileReference", "HideAdjustment", "image_reference",
            "ImageFileReference", "ImageReference", "Metadata", "motion_nodes", "parse", "properties", "Properties",
-           "RootAdjustment", "ShowAdjustment", "walk"]
+           "RootAdjustment", "ShowAdjustment", "VisibilityStatus", "walk"]
