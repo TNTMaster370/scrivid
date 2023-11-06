db240abf6cabe9c7652e7e23e936bc7320de7c3e
TNTMaster370 <trystynmcote@gmail.com>
2023-11-06 09:07:01 -0700

    Added a 'SeparatedInstructions' class to ._id_check;

--- scrivid/_id_check.py
+++ scrivid/_id_check.py
@@ -1,13 +1,17 @@
 from __future__ import annotations
 
 from . import errors
+from ._file_objects.images import ImageReference
 
 from typing import TYPE_CHECKING
 
+from sortedcontainers import SortedList
+
 if TYPE_CHECKING:
-    from ._file_objects.images import ImageReference
+    from ._file_objects.adjustments import RootAdjustment
 
     from collections.abc import Sequence
+    from typing import Dict, Hashable
 
     REFERENCES = ImageReference
 
@@ -15,6 +19,35 @@ if TYPE_CHECKING:
 def check_reference_id(references: Sequence[REFERENCES]):
     seen = set()
     for reference in references:
-        if reference.id in seen:
-            raise errors.DuplicateIDError(duplicate_id=reference.id)
-        seen.add(reference.id)
+        if not isinstance(reference, ImageReference):
+            continue
+        if reference.ID in seen:
+            raise errors.DuplicateIDError(duplicate_id=reference.ID)
+        seen.add(reference.ID)
+
+
+class SeparatedInstructions:
+    __slots__ = ("adjustments", "references")
+
+    adjustments: Dict[Hashable, SortedList[RootAdjustment]]
+    references: Dict[Hashable, ImageReference]
+
+    def __init__(self):
+        self.adjustments = {}
+        self.references = {}
+
+
+def separate_instructions(references: Sequence[REFERENCES]) -> SeparatedInstructions:
+    check_reference_id(references)
+    separated_instructions = SeparatedInstructions()
+
+    for reference in references:
+        if reference.ID in separated_instructions.references:
+            raise errors.DuplicateIDError(duplicate_id=reference.ID)
+        separated_instructions.references[reference.ID] = reference
+        for adjustment in reference.adjustments:
+            if reference.ID not in separated_instructions.adjustments:
+                separated_instructions.adjustments[reference.ID] = SortedList()
+            separated_instructions.adjustments[reference.ID].add(adjustment)
+
+    return separated_instructions
