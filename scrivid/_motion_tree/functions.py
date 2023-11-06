ba4d29e2a5f5de6d8a6e00396d46c6daefbeacc0
TNTMaster370 <trystynmcote@gmail.com>
2023-11-06 09:09:44 -0700

    Renamed ._id_check to ._separating_instructions;

--- scrivid/_motion_tree/functions.py
+++ scrivid/_motion_tree/functions.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from .nodes import Continue, End, HideImage, MotionTree, ShowImage, Start
 from .._file_objects.adjustments import HideAdjustment, ShowAdjustment
-from .._id_check import check_reference_id
+from .._separating_instructions import check_reference_id, separate_instructions, SeparatedInstructions
 from .._utils import sentinel
 
 from typing import TYPE_CHECKING
@@ -14,7 +14,7 @@ if TYPE_CHECKING:
     from .._file_objects.images import ImageReference
 
     from collections.abc import Sequence
-    from typing import Iterator, Union
+    from typing import Dict, Iterator, Union
 
     MOTION_NODES = Union[Continue, End, HideImage, MotionTree, ShowImage, Start]
     REFERENCES = ImageReference
@@ -27,77 +27,66 @@ def dump(motion_tree: MotionTree, *, indent: int = 0) -> str:
         return repr(motion_tree)
 
 
-def _create_command_node(reference_id, adjustment: RootAdjustment) -> Union[HideImage, ShowImage, None]:
+def _create_command_node(adjustment: RootAdjustment) -> Union[HideImage, ShowImage, None]:
     # INITIALIZE
     adjustment_type = type(adjustment)
     adjustment_time = adjustment.activation_time
+    relevant_id = adjustment.ID
 
     # OPERATION/TEARDOWN
     if adjustment_type == HideAdjustment:
-        return HideImage(reference_id, adjustment_time)
+        return HideImage(relevant_id, adjustment_time)
     elif adjustment_type == ShowAdjustment:
-        return ShowImage(reference_id, adjustment_time)
+        return ShowImage(relevant_id, adjustment_time)
     else:
         return None
 
 
-def _create_command_node_list(references: Sequence[REFERENCES]) -> SortedList[HideImage, ShowImage]:
-    # INITIALIZE
-    command_node_list = SortedList()
-
-    # OPERATION
-    for reference in references:
-        adjustments = reference.adjustments
-        for adjustment in adjustments:
-            command_node = _create_command_node(reference.id, adjustment)
-            if command_node is None:
-                raise TypeError
-            command_node_list.add(command_node)
-
-    # TEARDOWN
-    return command_node_list
-
-
-def _fill_motion_tree(motion_tree: MotionTree, command_node_list: SortedList[HideImage, ShowImage]):
-    # INITIALIZE
-    index = 0
-    UNFILLED = sentinel("UNFILLED")
-    current_node = UNFILLED
-
-    # OPERATION
-    while command_node_list:
-        if current_node is UNFILLED:
-            current_node = command_node_list.pop(0)
-
-        if current_node.time > index:
-            difference = current_node.time - index
-            motion_tree.body.append(Continue(difference))
-            index += difference
-            continue
-
-        motion_tree.body.append(current_node)
-        current_node = UNFILLED
-
-    if current_node is not UNFILLED:
-        motion_tree.body.append(current_node)
-
-
-def parse(references: Sequence[REFERENCES]) -> MotionTree:
-    # GATEKEEP
-    check_reference_id(references)
-
+def _create_motion_tree(separated_instructions: SeparatedInstructions) -> MotionTree:
     # INITIALIZE
     motion_tree = MotionTree()
-    motion_tree.body.append(Start())
-
-    command_node_list = _create_command_node_list(references)
 
     # OPERATION
-    _fill_motion_tree(motion_tree, command_node_list)
+    motion_tree.body.append(Start())
+
+    for node in _loop_over_adjustments(separated_instructions.adjustments):
+        motion_tree.body.append(node)
+
+    motion_tree.body.append(End())
 
     # TEARDOWN
-    motion_tree.body.append(End())
     return motion_tree
+
+
+def _loop_over_adjustments(adjustments: Dict[RootAdjustment]) -> Iterator[MOTION_NODES]:
+    sorted_adjustments = SortedList(
+        id_of_adj_value 
+        for adj_value in adjustments.values() 
+        for id_of_adj_value in adj_value
+    )
+    time_index = 0
+
+    for adjustment in sorted_adjustments:
+        current_node = _create_command_node(adjustment)
+
+        if current_node.time > time_index:
+            difference = current_node.time - time_index
+            yield Continue(difference)
+            time_index += difference
+
+        if current_node is None:
+            raise TypeError
+
+        yield current_node
+
+
+def parse(instructions: Union[Sequence[REFERENCES], SeparatedInstructions]) -> MotionTree:
+    if not isinstance(instructions, SeparatedInstructions):
+        instructions = separate_instructions(instructions)
+
+    check_reference_id(instructions.references)
+
+    return _create_motion_tree(instructions)
 
 
 def walk(motion_tree: MotionTree) -> Iterator[MOTION_NODES]:
